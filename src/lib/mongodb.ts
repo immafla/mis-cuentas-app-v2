785bdf065f5289eb698e58522fa2ae5d6d470461
imafla <mauroms_@live.com>
2026-02-22 10:46:18 -0500

    fix

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -21,7 +21,7 @@ const connectDB = async () => {
   }
   if (mongoose.connection.readyState >= 1) return;
   try {
-    await mongoose.connect(MONGODB_URI, { dbName: "MisCuentasApp-Dev" });
+    await mongoose.connect(MONGODB_URI, { dbName: "MisCuentasApp" });
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
